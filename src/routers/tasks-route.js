b7c28305e07ca3303a1b09e7637648a448043b57
Semiu <ayobami.sm@gmail.com>
2019-12-09 18:46:16 -0600

    task submission solved

--- src/routers/tasks-route.js
+++ src/routers/tasks-route.js
@@ -26,7 +26,6 @@ router.post('/tasks', auth, async (req, res) => {
     try {
         await task.save()
         res.status(201).send(task)
-        
 
     } catch (e) {
         res.status(400).send(e)
